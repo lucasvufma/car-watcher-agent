bd3eece95881900aad2e71f565a37fdce6da274d
Lucas <lucasvufma@gmail.com>
2019-09-24 19:31:06 -0300

    Setted basic logic and data for Environment, Agent and presenceSensor

---
+++ src/agent-environment.py
@@ -0,0 +1,65 @@
+class Thing:
+    self.position= None
+    def display(self , canvas, x, y, width, heigth):
+        pass
+    def position(self):
+        return self.position
+
+class car(Thing):
+    self.position=None
+    self.behaviour=None
+    def __init__(behaviour):
+        self.behaviour=behaviour
+    def setPosition(position):
+        self.position=position
+        
+class Agent:
+    x=0
+    y=0
+    def __init__(self,environment,presenceSensor):
+        self.perceptions= {}
+        self.environment=environment
+        self.presenceSensor=presenceSensor
+
+    """a ideia aqui é ter um dicionario com chaves representando as percepções e ele vá agindo perante a elas"""
+    def actuate(self):
+        for e in self.perceptions:
+            if self.perceptions[e]==self.environment.getState(e):
+                del(self.perceptions[e])
+                pass
+            else:
+                self.environment.setState(e,self.perceptions[e])
+                del(self.perceptions[e])
+    def percept(self):
+        self.perceptions=self.presenceSensor.getInformation()
+
+class presenceSensor:
+    def __init__(self,environment):
+        self.environment=environment
+        self.information = environment.park.copy()
+    def updateInformation(self):
+        for e in self.environment.park:
+            if e in self.information and self.information[e]==self.environment.park[e]:
+                del(self.information[e])
+            elif e in self.information and self.information[e]!=self.environment.park[e]:
+              self.information[e]=self.environment.park[e]
+            else:
+              self.information = self.environment.park.copy()
+    def getInformation(self):
+        self.updateInformation()
+        return print(self.information)
+
+
+class Environment:
+  def __init__(self):
+    self.things = []
+    self.park= {0:"free",1:"busy",2:"free",3:"busy"}
+  def setState(self,index,state):
+    self.park[index]=state
+  def getState(self,index):
+    return self.park[index]
+
+Environment1=Environment()
+presenceSensor1=presenceSensor(Environment1)
+Agent1=Agent(Environment1,presenceSensor1)
+Environment1.setState(0,"busy")
---
+++ src/agent.py
@@ -0,0 +1,10 @@
+class Thing:
+    self.position= None
+    def display(self , canvas, x, y, width, heigth):
+        pass
+    def position(self):
+        return self.position
+class Agent(Thing):
+    self.x=0
+    self.y=0
+    
